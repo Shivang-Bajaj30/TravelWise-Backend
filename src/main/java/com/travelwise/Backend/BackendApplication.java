59b5a68bbe4d4e9f126dcfcf2a77b19588ce6d35
Shivang-Bajaj30 <shivang.bajaj30@gmail.com>
2025-07-22 15:21:47 +0530

    ajnfad

--- src/main/java/com/travelwise/Backend/BackendApplication.java
+++ src/main/java/com/travelwise/Backend/BackendApplication.java
@@ -1,13 +1,17 @@
 package com.travelwise.Backend;
 
-import io.github.cdimascio.dotenv.Dotenv;
+// Remove or comment out this import if Dotenv is no longer used anywhere else
+// import io.github.cdimascio.dotenv.Dotenv;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class BackendApplication {
 	public static void main(String[] args) {
-		Dotenv dotenv = Dotenv.load();
+		// IMPORTANT: Remove or comment out the following line
+		// Dotenv dotenv = Dotenv.load(); // This line caused the "Could not find /.env" error
+
 		SpringApplication.run(BackendApplication.class, args);
 	}
 }
